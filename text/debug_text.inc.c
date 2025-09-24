45f6590dde940bdf20cd8640178b0fd7500f2487
super <smg5bruh1996@gmail.com>
2025-09-24 16:03:03 +0700

    wh wha

--- text/debug_text.inc.c
+++ text/debug_text.inc.c
@@ -14,9 +14,9 @@ static const u8 Debug0[] = {
 };
 
 static const u8 Debug1[] = {
-    _("ＰＡＵＳＥ　　　　\n"
-      "　つづける？\n"
-      "　やめる　？")
+    _("BOMBS?\n"
+      "YOU WANT IT?\n"
+      "IT'S YOURS MY FRIEND")
 };
 
 static const struct DialogEntry debug_text_entry_0 = {
@@ -24,7 +24,7 @@ static const struct DialogEntry debug_text_entry_0 = {
 };
 
 static const struct DialogEntry debug_text_entry_1 = {
-    1, 3, 100, 150, Debug1
+    1, 8, 100, 150, Debug1
 };
 
 const struct DialogEntry *const seg2_debug_text_table[] = {
